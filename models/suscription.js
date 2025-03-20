48a74748dafb8ae3309adc5d4f755cb8f2855fab
jua665 <117867140+jua665@users.noreply.github.com>
2025-03-20 07:38:27 -0600

    Update suscription.js

--- models/suscription.js
+++ models/suscription.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const SubscriptionSchema = new mongoose.Schema({
     endpoint: { type: String, required: true, unique: true },
-    expirationTime: { type: Date, default: null },
     keys: {
         p256dh: { type: String, required: true },
-        auth: { type: String, required: true }
+        auth: { type: String, required: true },
+        email: { type: String, required: true }
     },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
 });
